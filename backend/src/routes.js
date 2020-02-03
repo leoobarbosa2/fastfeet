cbf96e6dd4a343f03be58e15cdca0deebcf1ad64
Leonardo Barbosa <leonardo.pb@icloud.com>
2020-02-03 09:46:22 -0300

    Cadastro de destinatarios

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,7 @@ routes.use(authMiddleware);
 
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
+
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
